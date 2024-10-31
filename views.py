92156938399b1834dd1aee20747d6922fe31c4ed
Ola Adekoya <162782755+ThanosThumbs@users.noreply.github.com>
2024-10-30 23:14:57 -0500

    Add files via upload

---
+++ views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+import random
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def passgen(request):
+    char = list('abcdefghijklmnpqrstuvwxyz')
+    if request.GET.get('uppercase'):
+        char.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('digits'):
+        char.extend(list('123456789'))
+    if request.GET.get('symbols'):
+        char.extend(list('+-/!@#$%^&*~=?'))
+
+    length = int(request.GET.get('length', 10))
+
+    password = ""
+    for x in range(length):
+        password += random.choice(char)
+    return render(request, 'password.html', {'password': password})
